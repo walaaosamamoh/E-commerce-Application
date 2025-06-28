80db80fcebba8899191802c7263613979f23ef38
walaaosamamoh <waloosama@gmail.com>
2025-06-28 06:44:25 +0300

    feat: complete login page with validation, error handling and redirect

--- src/main.js
+++ src/main.js
@@ -9,10 +9,17 @@ import App from './App.vue';
 import router from './router';
 import './assets/main.css';
 import { createPinia } from 'pinia';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
 const app = createApp(App);
 const pinia = createPinia();
+const options ={
+  timeout: 3000,
+  position: 'top-right'
+}
 
+app.use(Toast, options)
 app.use(pinia);
 app.use(router);
 app.mount('#app');
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <VeeForm @submit.prevent="handleLogin" :validation-schema="schema">
+    <VeeForm @submit="handleLogin" :validation-schema="schema">
           <h2 class="text-2xl font-bold mb-4">Welcome back! Please log in</h2>
           <div>
             <Field name="email" type="email" placeholder="Email" class="border border-gray-400 outline-none w-full p-2 mb-2 rounded"/>
-            <ErrorMessage name="email" class="text-red-500 text-sm" />
+            <ErrorMessage name="email" class="text-red-500 text-sm"/>
           </div>
           <div>
-            <Field name="password" type="password" placeholder="Password" class="border border-gray-400 outline-none w-full p-2 mb-2 rounded"/>
+            <Field name="password" type="password" placeholder="Password" 
+            class="border border-gray-400 outline-none w-full p-2 mb-2 rounded"/>
             <ErrorMessage name="password" class="text-red-500 text-sm" />
           </div>
           <button type="submit" class="mt-2 w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">Login</button>
@@ -18,6 +19,9 @@
 <script>
 import { Form as VeeForm, Field, ErrorMessage } from 'vee-validate';
 import * as yup from 'yup';
+import {useAuthStore} from '@/stores/auth';
+import {mapState, mapActions} from 'pinia';
+import {useToast} from 'vue-toastification'
 
   export default {
     name: 'AdminLayout',
@@ -26,16 +30,69 @@ import * as yup from 'yup';
       Field,
       ErrorMessage
     },
+
+    computed:{
+      ...mapState(useAuthStore, [
+        'users',
+        'error',
+        'getUserByEmail'
+      ])
+    },
     data(){
       return{
         schema: yup.object({
           email: yup.string().email('Invalid email').required('Email is required'),
           password: yup.string().min(6, 'Password must be at least 6 characters').required('Password is required')
-        })
+        }),
+        user: null
       }
     },
+    created(){
+      this.toast= useToast()
+    },
     methods:{
-      handleLogin(){}
+      ...mapActions(useAuthStore, [
+        'fetchUsers'
+      ]),
+      handleLogin(values, {resetForm}){
+        // Validate form values
+        this.user = this.getUserByEmail(values.email)
+        // Check if user exists
+        if(!this.user){
+          // Show error message if user not found
+          this.toast.error('User not found',{
+            timeout: 2000,
+          })
+          // Reset form values
+          resetForm()
+          return
+        }
+        // show success message if user found
+        this.toast.success('Login successful',{
+            timeout: 1000,
+        })
+
+        // save user to localStorage
+        localStorage.setItem('user', JSON.stringify(this.user))
+
+        setTimeout(() => {
+          // Redirect user based on role
+          this.checkUserRole()
+        }, 1000)
+      },
+
+      // Check user role and redirect accordingly
+      checkUserRole(){
+        if(this.user && this.user.roleID === 'admin'){
+          this.$router.push({name:'dashboard'})
+        }else{
+          this.$router.push({name:'home'})
+        }
+      }
+    },
+    
+    mounted(){
+      this.fetchUsers()
     }
   }
 </script>

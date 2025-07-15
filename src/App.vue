1eceb1db17c830d7d9dff4309c5294ad0d50502b
walaaosamamoh <waloosama@gmail.com>
2025-07-15 03:37:41 +0300

    add language switcher dropdown to adminlayout and dir attribute to App

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
-  <div>
+  <div :dir="$i18n.locale === 'ar' ? 'rtl' : 'ltr'">
     <AppLayout />
   </div>
 </template>
 
 <script>
 import AppLayout from './components/layouts/AppLayout.vue'
-  
+
 export default {
   name: 'App',
   components: {
     AppLayout,
   },
-  mounted(){
-    document.dir = this.$i18n.locale === 'ar' ? 'rtl' : 'ltr';
-  }
 }
 </script>
--- src/components/layouts/AdminLayout.vue
+++ src/components/layouts/AdminLayout.vue
@@ -2,14 +2,25 @@
   <div class="min-h-screen flex flex-col">
     <header class="text-white shadow bg-blue-500 p-5 flex gap-2 justify-between items-center">
         <div class="flex items-center gap-2 ">
-          <img class="w-12 h-12 ml-4 rounded-full" 
+          <img class="w-12 h-12 ml-4 rounded-full"
             src="/src/assets/images/BlueCart_logo.jpg" alt="">
           <span class="text-2xl font-bold">BlueCart</span>
         </div>
-        <span class="material-icons cursor-pointer hover:text-red-500 transition"
-        @click="logOut">
+
+        <div class="flex items-center gap-4">
+        <!-- dropdown language menue -->
+        <select
+          v-model="$i18n.locale"
+          class="bg-blue-500 text-white rounded py-1 px-2 cursor-pointer outline-none"
+          @change="document.dir = $i18n.locale === 'ar' ? 'rtl' : 'ltr'"
+        >
+          <option value="en">English</option>
+          <option value="ar">العربية</option>
+        </select>
+        <span class="material-icons cursor-pointer hover:text-red-500 transition" @click="logOut">
           logout
         </span>
+      </div>
       </header>
     <main class="flex-2">
       <router-view />

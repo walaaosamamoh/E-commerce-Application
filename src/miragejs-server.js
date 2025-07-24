b1101db070c2555e6b76b3dbd43481c632995ee4
walaaosamamoh <waloosama@gmail.com>
2025-07-24 15:37:22 +0300

    Fix mirage blocking firebase requests by adding passthrough

--- src/miragejs-server.js
+++ src/miragejs-server.js
@@ -8,17 +8,6 @@ export function makeServer() {
   return createServer({
     models,
     seeds,
-    routes(){
-      this.passthrough([
-        'https://identitytoolkit.googleapis.com/**',
-        'https://securetoken.googleapis.com/**',
-        'https://e-commerce-a83c7.firebaseapp.com/**',
-        'https://e-commerce-a83c7.firebaseio.com/**',
-        'https://firestore.googleapis.com/**',
-      ]);
-
-      routes.call(this);
-    },
-    trackRequests: true
+    routes,
   });
 }
